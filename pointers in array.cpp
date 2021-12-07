ef5aa65f1bc8175579b4494e0332b3fb1ba5bafe
kartik tripathi <60856025+kartiktripathi2077@users.noreply.github.com>
2021-12-07 23:34:30 +0530

    Add files via upload

---
+++ pointers in array.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int a=10;
+    //int *aptr=&a;
+    
+    //cout<<*aptr<<endl;
+    //*aptr=20;
+    //cout<<a<<endl;
+    
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+    
+    
+    
+    return 0;
+}

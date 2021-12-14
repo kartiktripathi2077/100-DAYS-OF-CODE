2a6cbf8714a4ba0b87c44529661bbedd4f56baa2
kartik tripathi <60856025+kartiktripathi2077@users.noreply.github.com>
2021-12-14 23:42:19 +0530

    Add files via upload

---
+++ string with,without appending.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+  string str;
+  
+  string s1="fam";
+  string s2="ily";
+  
+  //s1.append(s2);
+  cout<<s1 + s2<<endl;
+	
+	return 0;
+}
